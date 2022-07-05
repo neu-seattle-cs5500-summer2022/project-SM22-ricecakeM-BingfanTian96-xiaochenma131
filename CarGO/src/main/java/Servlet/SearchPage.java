7536e1c833f290b11503a4147574bd378c719db1
BingfanTian96 <90421854+BingfanTian96@users.noreply.github.com>
2022-07-05 01:46:42 -0700

    Implement UserDA, CarDAO and ConnectionManager.

---
+++ CarGO/src/main/java/Servlet/SearchPage.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright © 2022 eSunny Info. Tech Ltd. All rights reserved.
+ * @Package: Servlet
+ * @author: bingfantian
+ * @date: 2022年7月5日 上午1:20:39 
+ */
+package Servlet;
+
+/**
+ * @ClassName: SearchPage
+ * @Description: TODO
+ * @author: bingfantian
+ * @date: 2022年7月5日 上午1:20:39
+ */
+public class SearchPage {
+
+}
